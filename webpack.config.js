d275b75b3932f354dd98ceb79e59f87f5b42be99
Samkiroko <kirokodev@gmail.com>
2020-07-21 01:57:06 +0300

    adding mode on config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'production',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
   },
 };
